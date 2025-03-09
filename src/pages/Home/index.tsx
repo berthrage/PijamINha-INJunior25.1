35ded1b7ba056f5f5d968bdb1749b23076fe03c1
Berth <miaberthrage@gmail.com>
2025-03-09 20:57:22 -0300

    feat(home): titleBox adicionada

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,26 @@
+import ImageLinkTransition from '../../components/ImageLinkTransition';
 import styles from './styles.module.css';
+import logoDark from '../../assets/images/logo-darkvariant.png';
+import logoDark2 from '../../assets/images/logo-darkvariant2.png';
 
 export default function Home() {
     return(
         <>
             <section className={styles.homeSection}>
-                <h1>Teste Home</h1>
+                
+                <div className={styles.titleBox}>
+                    <ImageLinkTransition
+                        firstImg={logoDark}
+                        secondImg={logoDark2}
+                        width={346}
+                        height={342.02}>
+                    </ImageLinkTransition>
+                    <div className={styles.sloganBox}>
+                        <h1>Se os lobos soubessem desse conforto, nem sopravam casas, iam dormir!</h1>
+                    </div>
+                </div>
+
+
             </section>
         </>
     )
