c988d30384218f75dd3f421e12eef0ff434a3526
Douglas Cristiano <164531720+DougCristiano@users.noreply.github.com>
2025-03-10 17:11:17 -0300

    Fix: Login com tag correta

--- src/components/FormContainer/index.tsx
+++ src/components/FormContainer/index.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 import styles from "./styles.module.css";
 
-interface FormConainerProps extends React.HTMLAttributes<HTMLDivElement> {
+interface FormContainerProps extends React.HTMLAttributes<HTMLFormElement> {
     children: React.ReactNode;
     title: string;
-    description?: string;
+    description?: any;
 }
 
-const Button: React.FC<FormConainerProps> = ({ children, title, description, ...rest}) => {
+const FormContainer: React.FC<FormContainerProps> = ({ children, title, description, ...rest}) => {
     return (
-        <div className={styles.FormContainer}>
+        <form className={styles.FormContainer} {...rest}>
             <div className={styles.internalContainer1}>
                 <h1 className={styles.titleFormContainer}>{title}</h1>
                 <p className={styles.descriptionFormContainer}>{description}</p>
             </div>
-            <div className={styles.internalContainer2} {...rest}>{children}</div>
-        </div>
+            <div className={styles.internalContainer2}>{children}</div>
+        </form>
     );
 };
 
-export default Button;
+export default FormContainer;
