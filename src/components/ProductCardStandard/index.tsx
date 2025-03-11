d024540e1a42e6c0595035f33aad1593cd799dc2
Berth <miaberthrage@gmail.com>
2025-03-11 10:24:13 -0300

    feat(ProductCardStandard): preloading de heartActiveHovered

--- src/components/ProductCardStandard/index.tsx
+++ src/components/ProductCardStandard/index.tsx
@@ -25,6 +25,7 @@ export default function ProductCardStandard({pajama}: ProductCardStandardProps) 
         };
 
         preloadImage(heartActive);
+        preloadImage(heartActiveHovered);
         preloadImage(heartInactive);
     }, []);
 
