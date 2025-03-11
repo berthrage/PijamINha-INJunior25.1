e6abec25b6e0d335dcae37c0f3f89e1abf8fce14
Berth <miaberthrage@gmail.com>
2025-03-11 06:54:07 -0300

    feat(RatingCard): Estrelas

--- src/components/RatingCard/index.tsx
+++ src/components/RatingCard/index.tsx
@@ -1,17 +1,37 @@
 import Feedback from '../../types/Feedback';
 import styles from './styles.module.css';
+import starInactive from '../../assets/icons/star-inactive.png';
+import starActive from '../../assets/icons/star-active.png';
+import starHalf from '../../assets/icons/star-active-half.png';
+
 
 interface RatingCardProps {
     feedback: Feedback,
 }
 
 export default function RatingCard({ feedback }: RatingCardProps) {   
+    const renderStars = (rating: number) => {
+        const stars = [];
+        for (let i = 1; i <= 5; i++) {
+            if (i <= rating) {
+                stars.push(<img key={i} src={starActive} alt="Active Star" className={styles.star} />);
+            } else if (i - rating === 0.5) {
+                stars.push(<img key={i} src={starHalf} alt="Half Star" className={styles.star} />);
+            } else {
+                stars.push(<img key={i} src={starInactive} alt="Inactive Star" className={styles.star} />);
+            }
+        }
+        return stars;
+    };
+
     return (
         <>
             <div className={styles.ratingCard}>
                 <div className={styles.ratingCardHeader}>
                     <h1>{feedback.name}</h1>
-                    <h2>{feedback.rating}</h2>
+                    <div className={styles.stars}>
+                        {renderStars(feedback.rating)}
+                    </div>
                 </div>
                 <div className={styles.ratingCardDescription}>
                     <p>{feedback.description}</p>
