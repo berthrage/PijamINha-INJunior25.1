3337a82318af171c0208e92726692c000650e0e0
Berth <miaberthrage@gmail.com>
2025-03-11 04:35:24 -0300

    feat(RatingCardCarousel): Melhorias nas Custom Arrows (com ImageLinkTransition), loop mode

--- src/components/RatingCardsCarousel/index.tsx
+++ src/components/RatingCardsCarousel/index.tsx
@@ -4,11 +4,13 @@ import Feedback from "../../types/Feedback";
 import RatingCard from "../RatingCard";
 import styles from './styles.module.css';
 import arrowNext from '../../assets/icons/next.png';
+import arrowNextHovered from '../../assets/icons/next-hovered.png';
 import arrowPrev from '../../assets/icons/prev.png';
-
+import arrowPrevHovered from '../../assets/icons/prev-hovered.png';
 import "swiper/swiper-bundle.css";
 import "swiper/css";
 import "swiper/css/navigation";
+import ImageLinkTransition from "../ImageLinkTransition";
 
 interface RatingCardsCarouselProps {
     feedbacks: Feedback[];
@@ -17,26 +19,36 @@ interface RatingCardsCarouselProps {
 export default function RatingCardsCarousel({ feedbacks }: RatingCardsCarouselProps) {
     return (
         <div className={styles.ratingCardsCarousel}>
-            <div className={styles.swiperButtonPrev}>
-                <img src={arrowPrev} alt="Previous" />
-            </div>
-            <div className={styles.swiperButtonNext}>
-                <img src={arrowNext} alt="Next" />
-            </div>
+            <ImageLinkTransition id={styles.swiperButtonPrev}
+                firstImg={arrowPrev}
+                secondImg={arrowPrevHovered}
+                firstAlt="prev"
+                secondAlt="prev-hovered">
+            </ImageLinkTransition>
+            <ImageLinkTransition id={styles.swiperButtonNext}
+                firstImg={arrowNext}
+                secondImg={arrowNextHovered}
+                firstAlt="next"
+                secondAlt="next-hovered">
+            </ImageLinkTransition>
 
             <Swiper
                 modules={[Navigation]} // Enable navigation
-                spaceBetween={20} // Space between slides
+                loop={true} 
+                spaceBetween={1} // Space between slides
                 slidesPerView={1} // Default slides per view
+                centerInsufficientSlides={true}
+                centeredSlidesBounds={true}
                 navigation={{
-                    nextEl: `.${styles["swiperButtonNext"]}`,
-                    prevEl: `.${styles["swiperButtonPrev"]}`,
+                    nextEl: `#${styles["swiperButtonNext"]}`,
+                    prevEl: `#${styles["swiperButtonPrev"]}`,
                 }} // Enable custom navigation arrows
                 breakpoints={{
                     640: { slidesPerView: 1 },
-                    768: { slidesPerView: 2 },
-                    1024: { slidesPerView: 3 },
+                    945: { slidesPerView: 2 },
+                    1413: { slidesPerView: 3 },
                 }}
+                
                 className={styles.swiperContainer}
             >
                 {feedbacks.map((feedback, index) => (
