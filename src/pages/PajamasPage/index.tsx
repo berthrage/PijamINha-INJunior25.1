91c76cd5b7f1aec47da8fa5961d835146d58062c
Berth <miaberthrage@gmail.com>
2025-03-12 21:20:51 -0300

    feat: Componente PaginationCarousel

--- src/pages/PajamasPage/index.tsx
+++ src/pages/PajamasPage/index.tsx
@@ -2,13 +2,14 @@ import { useEffect, useRef, useState } from 'react';
 import usePajamasStore from '../../stores/PajamasStore';
 import styles from './styles.module.css';
 import ProductCardStandard from '../../components/ProductCardStandard';
+import PaginationCarousel from '../../components/PaginationCarousel';
 
 export default function PajamasPage() {
     const { pajamas, fetchPajamas, errorCode } = usePajamasStore();
     const [itemsPerRow, setItemsPerRow] = useState(4);
     const initialPage = localStorage.getItem('pajamasCurrentPage')? parseInt(localStorage.getItem('pajamasCurrentPage')!) : 1;
     const [currentPage, setCurrentPage] = useState(initialPage);
-    const itemsPerPage = 12;
+    const itemsPerPage = 9;
     const listContainerRef = useRef<HTMLDivElement>(null);
     const cardScale = 0.9;
 
@@ -66,30 +67,16 @@ export default function PajamasPage() {
                         )}
                     </ul>
                 </div>
-                <div className={styles.pagination}>
-                    <button 
-                        onClick={() => {
-                                            setCurrentPage(prev => Math.max(prev - 1, 1));
-                                            localStorage.setItem('pajamasCurrentPage', (currentPage - 1).toString());
-                                        }} 
-                        disabled={currentPage === 1}
-                    >
-                        Previous
-                    </button>
-                    <span>Page {currentPage} of {totalPages}</span>
-                    <button 
-                        onClick={() => {
-                                            setCurrentPage(prev => Math.min(prev + 1, totalPages));
-                                            localStorage.setItem('pajamasCurrentPage', (currentPage + 1).toString());
-                                        }} 
-                        disabled={currentPage === totalPages}
-                    >
-                        Next
-                    </button>
-                </div>
-
-                <div className={styles.paginationCarousel}>
-                    
+                                                    
+                <div className={styles.paginationCarouselContainer}>
+                    <PaginationCarousel
+                        totalPages={totalPages}
+                        currentPage={currentPage}
+                        onClickGoToPage={(value) => {
+                            setCurrentPage(value);
+                            localStorage.setItem('pajamasCurrentPage', (value).toString());
+                        }}>
+                    </PaginationCarousel>
                 </div>
             </div>
         </>
