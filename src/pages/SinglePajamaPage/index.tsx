0a190400e4c5035eca7c09f0fa0f1c38e01b260b
Berth <miaberthrage@gmail.com>
2025-03-14 07:16:23 -0300

    feat(SinglePajamaPage): FadeInEffect e Icones de Filtros/Features

--- src/pages/SinglePajamaPage/index.tsx
+++ src/pages/SinglePajamaPage/index.tsx
@@ -4,8 +4,12 @@ import styles from './styles.module.css'
 import { FaHeart } from 'react-icons/fa'
 import ImageLink from '../../components/ImageLink'
 import inverno from '../../assets/icons/winter-grouped.png'
+import verao from '../../assets/icons/summer-grouped.png'
 import unissex from '../../assets/icons/unisex-grouped.png'
+import feminino from '../../assets/icons/female-sign-grouped.png';
+import masculino from '../../assets/icons/male-sign-grouped.png';
 import adulto from '../../assets/icons/foradults-grouped.png'
+import infantil from '../../assets/icons/forkids-grouped.png'
 import { useParams } from 'react-router-dom'
 import usePajamasStore from '../../stores/PajamasStore'
 import { use, useEffect, useLayoutEffect, useRef, useState } from 'react'
@@ -14,16 +18,37 @@ import PriceRealFormatted from '../../components/PriceRealFormatted'
 import useMediaQuery from '../../hooks/useMediaQueries'
 
 export default function SinglePajamaPage() {
-    const isWideEnough = useMediaQuery('(min-width: 440px)');
+    // const isWideEnough = useMediaQuery('(min-width: 440px)');
+    const [isVisible, setIsVisible] = useState(false);
     const { pajamaName } = useParams<{ pajamaName: string }>();
     const decodedPajamaName = decodeURIComponent(pajamaName || '');
     const { pajamas, fetchPajamas, errorCode } = usePajamasStore();
     const [ pajama, setPajama ] = useState<Pajama>();
-    const containerPajamaRef = useRef<HTMLDivElement>(null);
+    const scrollRef = useRef<HTMLDivElement>(null);
+    const pajamaContainerRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         fetchPajamas();
     }, [fetchPajamas]);
+
+    // Observer for Fade-in Effect
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                if (entries[0].isIntersecting) {
+                    setTimeout(() => {
+                        setIsVisible(true);
+                    }, 50);
+                    observer.disconnect();
+                }
+            }, { threshold: 0.1 } 
+        );
+
+        if (pajamaContainerRef.current) {
+            observer.observe(pajamaContainerRef.current);
+        }
+        return () => observer.disconnect();
+    }, [pajama]);
 
     useEffect(() => {
         if (pajamas.length > 0) {
@@ -36,10 +61,10 @@ export default function SinglePajamaPage() {
 
     // Scroll to the height of the containerIndividualPajama div on load
     useLayoutEffect(() => {
-        if (containerPajamaRef.current) {
+        if (scrollRef.current) {
             setTimeout(() => {
-                if (containerPajamaRef.current) {
-                    window.scrollTo(0, containerPajamaRef.current.offsetTop - 50 || 0);
+                if (scrollRef.current) {
+                    window.scrollTo(0, scrollRef.current.offsetTop - 50 || 0);
                 }
             }, 0);
         }
@@ -60,6 +85,31 @@ export default function SinglePajamaPage() {
         setSelectedSizeStock(pajama?.sizes.find((size) => size.size === selectedSize)?.stock_quantity || 0);
     } , [selectedSize, pajama]);
 
+    const genderImage = () => {
+        if (pajama?.gender === 'Unissex' || pajama?.gender === 'Infantil') {
+            return unissex;
+        } else if (pajama?.gender === 'Feminino') {
+            return feminino;
+        } else if (pajama?.gender === 'Masculino') {    
+            return masculino;
+        }
+        return ''; 
+    };
+
+    const forAdultsKidsImage = () => {
+        if (pajama?.gender === 'Infantil') {
+            return infantil;
+        }
+        return adulto;
+    }
+
+    const seasonImage = () => {
+        if (pajama?.season === 'Inverno' || pajama?.season === 'Outono') {
+            return inverno;
+        }
+        return verao;
+    }
+
     return (
         <>
             <div className={styles.singlePajamaSection}>
@@ -68,94 +118,103 @@ export default function SinglePajamaPage() {
                     <h1>{errorCode ? `Erro ${errorCode} ao carregar pijama` : 'Carregando pijamas...'}</h1>
                 ) : pajama ? (
                     <>
-                        <div className={styles.containerPajama}>
+                        <div className={`${styles.pajamaContainer} ${isVisible ? styles.fadeIn : ""}`} ref={pajamaContainerRef}>
 
-                            <ImageLink
-                                img={pajama.image}
-                                width={550}
-                                id={styles.pajamaImage}>
-                            </ImageLink>
-                            <div className={styles.containerInformation}>
+                            <div className={styles.pajamaMainSection}>
 
-                                <div className={styles.titleInformation}>
-                                    <h1>{pajama.name}</h1>
-                                    <p>Ref: #123456</p>
-                                </div>
+                                <ImageLink
+                                    img={pajama.image}
+                                    width={550}
+                                    id={styles.pajamaImage}>
+                                </ImageLink>
 
-                                <div className={styles.priceInformation}  ref={containerPajamaRef}>
-                                    <div>
-                                        <h3>{<PriceRealFormatted price={pajama.price}></PriceRealFormatted>}</h3>
-                                        <p>Pix</p>
+                                <div className={styles.containerInformation}>
+
+                                    <div className={styles.titleInformation}>
+                                        <h1>{pajama.name}</h1>
+                                        <p>Ref: #123456</p>
                                     </div>
-                                    <p>Parcelamento</p>
-                                </div>
 
-                                <div className={styles.sizeInformation}>
-                                    <p>Tamanhos:</p>
-                                    <div className={styles.buttonInformation}>
-                                        {pajama.sizes.length > 0 && allSizesHaveStock ? pajama.sizes.map(size => (
-                                            <button 
-                                                key={size.size}
-                                                onClick={() => {
-                                                    setSelectedSize(size.size);
-                                                    console.log(size.size);
-                                                }}
-                                                className={size.size === selectedSize ? styles.sizeButtonActive :
-                                                    styles.sizeButtonInactive
-                                                }>
+                                    <div className={styles.priceInformation} ref={scrollRef}>
+                                        <div>
+                                            <h3>{<PriceRealFormatted price={pajama.price}></PriceRealFormatted>}</h3>
+                                            <p>Pix</p>
+                                        </div>
+                                        <p>Parcelamento</p>
+                                    </div>
+
+                                    <div className={styles.sizeInformation}>
+                                        <p>Tamanhos:</p>
+                                        <div className={styles.buttonInformation}>
+                                            {pajama.sizes.length > 0 && allSizesHaveStock ? pajama.sizes.map(size => (
+                                                <button
+                                                    key={size.size}
+                                                    onClick={() => {
+                                                        setSelectedSize(size.size);
+                                                        console.log(size.size);
+                                                    }}
+                                                    className={size.size === selectedSize ? styles.sizeButtonActive :
+                                                        styles.sizeButtonInactive
+                                                    }>
                                                     {size.size}
                                                 </button>
-                                        )) : (
-                                            <p>Estoque indisponível.</p>
-                                        )}
+                                            )) : (
+                                                <p>Estoque indisponível.</p>
+                                            )}
+                                        </div>
+                                        <span>
+                                            {pajama.sizes.find((size) => size.size === selectedSize)?.stock_quantity === 0 || selectedSize === '' ? 'Estoque esgotado!' : (<>
+                                                Ainda temos {selectedSizeStock} peças do tamanho escolhido em nosso estoque!
+                                            </>)}
+                                        </span>
                                     </div>
-                                    <span>
-                                        {pajama.sizes.find((size) => size.size === selectedSize)?.stock_quantity === 0 || selectedSize === '' ? 'Estoque esgotado!' : (<>
-                                            Ainda temos {selectedSizeStock} peças do tamanho escolhido em nosso estoque!
-                                        </>) }
-                                    </span>
-                                </div>
 
-                                <div className={styles.quantityInformation}>
-                                    <p>Quantidade:</p>
-                                    <NumericStepper
-                                        quantity={1}></NumericStepper>
-                                </div>
+                                    <div className={styles.quantityInformation}>
+                                        <p>Quantidade:</p>
+                                        <NumericStepper
+                                            quantity={1}></NumericStepper>
+                                    </div>
 
-                                <div className={styles.addcartandwishlistInformation}>
-                                    <Button id={styles.buttonIndividualPajama}>Adicionar ao Carrinho</Button>
-                                    <FaHeart className={styles.iconactive} size={86}></FaHeart>
-                                </div>
+                                    <div className={styles.addcartandwishlistInformation}>
+                                        <Button id={styles.buttonIndividualPajama}>Adicionar ao Carrinho</Button>
+                                        <FaHeart className={styles.iconactive} size={86}></FaHeart>
+                                    </div>
 
+                                </div>
+                            </div>
+
+                            <div className={styles.featuresSection}>
+                                <ul className={styles.featuresList}>
+                                    <li>
+                                        <ImageLink
+                                            img={seasonImage()}
+                                            width={70}
+                                            height={70}
+                                            id={styles.featureImage}>
+                                        </ImageLink>
+                                    </li>
+
+                                    <li>
+                                        <ImageLink
+                                            img={genderImage()}
+                                            width={70}
+                                            height={70}
+                                            id={styles.featureImage}>
+                                        </ImageLink>
+                                    </li>
+
+                                    <li>
+                                        <ImageLink
+                                            img={forAdultsKidsImage()}
+                                            width={70}
+                                            height={70}
+                                            id={styles.featureImage}>
+                                        </ImageLink>
+                                    </li>
+                                </ul>
                             </div>
                         </div>
-                        <div className={styles.featuresSection}>
-                            <ul className={styles.featuresList}>
-                                <li>
-                                    <ImageLink
-                                        img={inverno}
-                                        width={70}
-                                        height={70}>
-                                    </ImageLink>
-                                </li>
 
-                                <li>
-                                    <ImageLink
-                                        img={unissex}
-                                        width={70}
-                                        height={70}>
-                                    </ImageLink>
-                                </li>
-
-                                <li>
-                                    <ImageLink
-                                        img={adulto}
-                                        width={70}
-                                        height={70}>
-                                    </ImageLink>
-                                </li>
-                            </ul>
-                        </div>
 
                         <div className={styles.aboutSection}>
                             <div className={styles.titleaboutSection}>
@@ -167,8 +226,8 @@ export default function SinglePajamaPage() {
                                     <ul><li>Uma blusa de mangas longas na cor azul petróleo com estampa poá branca</li><li>Uma calça na cor azul petróleo com estampa poá branca</li></ul>
                                     <h4>Composição:</h4>
                                     <ul><li>100% algodão</li></ul>
-                                    </div>
                                 </div>
+                            </div>
 
                         </div>
                     </>
