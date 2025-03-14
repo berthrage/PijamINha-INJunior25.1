8a9ddbd9b4ddda3111659461e42afb19a12778b2
Berth <miaberthrage@gmail.com>
2025-03-14 11:30:36 -0300

    feat: Integração ProductCardsCarousel e FavoritesPage

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,11 +1,22 @@
+import { useEffect } from 'react';
+import usePajamasStore from '../../stores/PajamasStore';
 import styles from './styles.module.css';
+import ProductCardsCarousel from '../../components/ProductCardsCarousel';
 
 export default function Favorites() {
+    const { pajamas, fetchPajamas } = usePajamasStore();
+
+    useEffect(() => {
+        fetchPajamas();
+    }, [fetchPajamas]);
+
+    const favoritePajamas = pajamas.filter(pajama => pajama.favorite);
+
     return (
         <>
             <section className={styles.favoritesSection}>
                 <div>
-                    <p>lalala</p>
+                    <ProductCardsCarousel pajamas={favoritePajamas}/>
                 </div>
             </section>
         </>
